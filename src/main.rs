72bd0b260c16e5c7f63a00fe2ca3a0521bd5b016
Lucas Simon <lucasimonmendes@hotmail.com>
2024-02-19 10:18:49 -0300

    :sparkles: feat: add menu module in tools trail

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-// use dialoguer::Select;
-// use std::collections::HashMap;
-//use ui::Menu;
-// use termimad;
-
 use std::collections::BTreeMap;
 
 mod markdown_render;
@@ -55,42 +50,6 @@ fn main() {
     );
     // Exibição do menu principal
     ui::print_menu(&main_menu);
-
-    /*
-    let menu = Select::new()
-        .item("Sobre o jogo")
-        .item("Trilha Tools")
-        .item("Trilha Web Dev")
-        .item("Regras")
-        .item("Sair")
-        .default(0)
-        .interact()
-        .unwrap();
-
-    match menu {
-        0 => {
-            termimad::print_text("**CodeQuest** é o jogo para te ensinar a **programar**\n com este ambiente de desenvolvimento,\n utilizando **terminal** e ferramentas do terminal,\n como **Tmux**, **Neovim**, **Git e GitHub**. \n");
-            std::process::exit(0);
-        }
-        1 => {
-            tools::tools();
-        }
-        2 => {
-            web::web_dev();
-        }
-        3 => {
-            termimad::print_text(
-                "As regras são:\n- **não** use chatgpt.\n- use **apenas o que foi fornecido** a você.\n"
-            );
-            std::process::exit(0);
-        }
-        4 => {
-            println!("Saindo...");
-            std::process::exit(0);
-        }
-        _ => println!("Escolha inválida!"),
-    }
-    */
 }
 
 fn about() {
